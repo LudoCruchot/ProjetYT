ca0e82783a0c5615a4c2d3db77dad41568abad27
LudoCruchot <32196302+LudoCruchot@users.noreply.github.com>
2018-03-25 16:00:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/simpleplayer/PlayerActivity.java
@@ -0,0 +1,88 @@
+package com.example.simpleplayer;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+/**
+ * Created by Antoine on 17/03/2018.
+ */
+
+
+
+public class PlayerActivity extends YouTubeBaseActivity implements OnInitializedListener {
+
+    private YouTubePlayerView playerView;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.activity_player);
+
+        playerView = (YouTubePlayerView)findViewById(R.id.player_view);
+        playerView.initialize(YoutubeConnector.KEY, this);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult result) {
+        Toast.makeText(this, getString(R.string.failed), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        player.setPlayerStateChangeListener(playerStateChangeListener);
+        player.setPlaylistEventListener((YouTubePlayer.PlaylistEventListener) playbackEventListener);
+
+        if(!wasRestored){
+            player.cueVideo(getIntent().getStringExtra("VIDEO_ID"));
+        }
+    }
+
+    private PlaybackEventListener playbackEventListener = new PlaybackEventListener() {
+        @Override
+        public void onBuffering(boolean arg0) {
+        }
+        @Override
+        public void onPaused() {
+        }
+        @Override
+        public void onPlaying() {
+        }
+        @Override
+        public void onSeekTo(int arg0) {
+        }
+        @Override
+        public void onStopped() {
+        }
+    };
+    private PlayerStateChangeListener playerStateChangeListener = new PlayerStateChangeListener() {
+        @Override
+        public void onAdStarted() {
+        }
+        @Override
+        public void onError(YouTubePlayer.ErrorReason arg0) {
+        }
+        @Override
+        public void onLoaded(String arg0) {
+        }
+        @Override
+        public void onLoading() {
+        }
+        @Override
+        public void onVideoEnded() {
+        }
+        @Override
+        public void onVideoStarted() {
+        }
+    };
+}
